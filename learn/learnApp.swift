3cdd68d9a51beb0ff96bcbcdd7edf29ae30d0177
Cezar Diaconu <alfacez2003@gmail.com>
2026-06-02 12:12:41 +0300

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by Cezar Diaconu on 02.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
